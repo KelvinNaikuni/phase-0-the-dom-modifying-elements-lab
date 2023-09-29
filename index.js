efad7204e083227458b76025c43065360fc976ba
KelvinNaikuni <kelvin.naikuni@student.moringaschool.com>
2023-09-29 07:11:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+document.querySelector("#main").remove();
+
+const victoryHeader = document.querySelector("h1#victory");
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+newHeader.textContent = 'Kelvin is the champion';
+
+
+
+
+
